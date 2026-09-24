7ef9489e5d1bfabc9eba3035c377e9f75517d052
frazeui <farhadsiddique5@gmail.com>
2026-09-24 09:50:02 +0500

    updated

---
+++ llmops_tracing/tracing_demo.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+
+llm=ChatGroq(
+    model="openai/gpt-oss-20b",
+    temperature=0
+)
+
+
+response=llm.invoke("Explain transaction fraud detection in one sentence.")
+
+print(response.content)
